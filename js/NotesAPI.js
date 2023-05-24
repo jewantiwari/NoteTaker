0b562f1e0338f9d54ac541dacc5d52e675ed6709
Jack Tiwari <jack.jewan@gmail.com>
2023-05-24 15:19:50 -0400

    Few Updates

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -1,35 +1,34 @@
 export default class NotesAPI {
-    static getAllNotes(){
-        const notes = JSON.parse(localStorage.getItem("noteTakerApp-notes")|| "[]");
+    static getAllNotes() {
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
 
-        return notes.sort((a,b) => {
-            return new Date(a.updated) > new Date(b.updated)?-1:1;
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
         });
     }
-    static saveNote(noteToSave){
+
+    static saveNote(noteToSave) {
         const notes = NotesAPI.getAllNotes();
         const existing = notes.find(note => note.id == noteToSave.id);
 
         // Edit/Update
-       if (existing){
-        existing.title = noteToSave.title;
-        existing.body = noteToSave.body;
-        existing.updated = new Date().toISOString();
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
 
-       } else { 
-        noteToSave.id = Math.floor(Math.random()*1000000);
-        noteToSave.update = new Date().toISOString();
-        notes.push(noteToSave);
-
-       } 
-       
-        localStorage.setItem("noteTakerApp-notes", JSON.stringify(notes));
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
-    static deleteNote(id){
+
+    static deleteNote(id) {
         const notes = NotesAPI.getAllNotes();
-        const newNotes = notes.filter(note => note.id !=id);
+        const newNotes = notes.filter(note => note.id != id);
 
-        localStorage.setItem("noteTakerApp-notes", JSON.stringify(newNotes));
-
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
     }
-} 
+}
